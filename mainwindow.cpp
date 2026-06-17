9c9a0595a8e11cc9074ac104661c078dbbb78641
Nikita Pershin <pershin@delta-vrn.ru>
2026-06-17 17:59:16 +0300

    Реализован парсинг буффера и отправка команд на сторону сервера

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -3,6 +3,7 @@
 #include <QHBoxLayout>
 #include <QHeaderView>
 #include <QDebug>
+#include <vector>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -15,6 +16,7 @@ MainWindow::MainWindow(QWidget *parent)
     connect(m_tcpSocket, &QTcpSocket::errorOccurred, this, &MainWindow::errorHandle);
 
     connect(m_addUserButton, &QPushButton::clicked, this, &MainWindow::onAddUserButtonClicked);
+    connect(m_refreshTableButton, &QPushButton::clicked, this, &MainWindow::onRefreshClicked);
 
     m_tcpSocket->connectToHost("127.0.0.1", 12345);
 
@@ -40,6 +42,7 @@ void MainWindow::setupUI()
     formLayout->addWidget(m_emailInput);
 
     m_addUserButton = new QPushButton("Добавить пользователя", this);
+    m_refreshTableButton = new QPushButton("Обновить таблицу", this);
 
     m_table = new QTableWidget(this);
     m_table->setColumnCount(3);
@@ -48,6 +51,7 @@ void MainWindow::setupUI()
 
     mainLayout->addLayout(formLayout);
     mainLayout->addWidget(m_addUserButton);
+    mainLayout->addWidget(m_refreshTableButton);
     mainLayout->addWidget(m_table);
 
     setCentralWidget(centralWidget);
@@ -56,9 +60,103 @@ void MainWindow::setupUI()
     resize(600, 400);
 }
 
+void MainWindow::sendCommand(const json& j)
+{
+    if(m_tcpSocket->state() == QTcpSocket::UnconnectedState)
+    {
+        m_tcpSocket->connectToHost("127.0.0.1", 12345);
+    }
+
+    if (m_tcpSocket->state() == QAbstractSocket::ConnectedState)
+    {
+        std::string requestStr = j.dump() + "\n";
+        m_tcpSocket->write(requestStr.c_str(), requestStr.length());
+        m_tcpSocket->flush();
+    }
+}
+
 void MainWindow::parseResponse(const json& j)
 {
+    std::string status = j.value("status", "");
 
+    // status: error, ok(ping pong), success
+    if (status != "success")
+    {
+        std::string err = j.value("message", "");
+        showErrorMessage(err);
+    }
+
+    if (j.contains("users") && j["users"].is_array())
+    {
+        std::vector<User> receivedUsers;
+        for (const auto& jsonUser : j["users"])
+        {
+            User user;
+            user.id = jsonUser.value("id", 0);
+            user.username = jsonUser.value("username", "");
+            user.email = jsonUser.value("email", "");
+            receivedUsers.push_back(user);
+        }
+        renderUserTable(receivedUsers);
+    }
+    else
+    {
+        std::string answer = j.value("message", "Пользователь успешно добавлен");
+        showSuccessMessage(answer);
+    }
+
+
+}
+
+void MainWindow::showErrorMessage(const std::string &err)
+{
+
+}
+
+void MainWindow::renderUserTable(const std::vector<User> &users)
+{
+    m_table->setRowCount(0);
+    int row = 0;
+    for (const auto& user : users) {
+        m_table->insertRow(row);
+        m_table->setItem(row, 0, new QTableWidgetItem(QString::number(user.id)));
+        m_table->setItem(row, 1, new QTableWidgetItem(QString::fromStdString(user.username)));
+        m_table->setItem(row, 2, new QTableWidgetItem(QString::fromStdString(user.email)));
+        row++;
+    }
+}
+
+void MainWindow::showSuccessMessage(const std::string &answer)
+{
+
+}
+
+void MainWindow::onAddUserButtonClicked()
+{
+    QString username = m_usernameInput->text().trimmed();
+    QString email = m_emailInput->text().trimmed();
+
+    if(username.isEmpty() || email.isEmpty())
+    {
+        showErrorMessage("Пожалуйста, заполните все поля!");
+    }
+
+    json j;
+    j["action"] = "add_user";
+    j["username"] = username.toStdString();
+    j["email"] = email.toStdString();
+
+    m_usernameInput->clear();
+    m_emailInput->clear();
+
+    sendCommand(j);
+}
+
+void MainWindow::onRefreshClicked()
+{
+    json j;
+    j["action"] = "get_users";
+    sendCommand(j);
 }
 
 void MainWindow::onReadyRead()
@@ -79,11 +177,16 @@ void MainWindow::onReadyRead()
             }
             catch (const json::parse_error& e)
             {
-                qDebug << "Error parse json from server" << e.what();
+                qDebug() << "Error parse json from server: " << e.what();
             }
 
         }
     }
+}
+
+void MainWindow::errorHandle()
+{
+
 }
 
 
--- mainwindow.h
+++ mainwindow.h
@@ -12,6 +12,13 @@
 
 using json = nlohmann::json;
 
+struct User
+{
+    int id;
+    std::string username;
+    std::string email;
+};
+
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
@@ -25,15 +32,21 @@ private:
     QLineEdit* m_usernameInput;
     QLineEdit* m_emailInput;
     QPushButton* m_addUserButton;
+    QPushButton* m_refreshTableButton;
     QTableWidget* m_table;
 
     QTcpSocket* m_tcpSocket;
     QByteArray m_buffer;
 
+    void sendCommand(const json& j);
     void parseResponse(const json& j);
+    void showErrorMessage(const std::string& err);
+    void renderUserTable(const std::vector<User>& users);
+    void showSuccessMessage(const std::string& answer);
 
 private slots:
     void onAddUserButtonClicked();
+    void onRefreshClicked();
     void onReadyRead();
     void errorHandle();
 };
